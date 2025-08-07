3b23f0345f8852dacd12a5f50dc3939f088d9a32
ayushrathour <ayushrathour2910@gmail.com>
2025-08-08 00:54:32 +0530

    Update vite.config.js

--- client/portfolio-dashboard/vite.config.js
+++ client/portfolio-dashboard/vite.config.js
@@ -4,6 +4,14 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()], 
+  plugins: [react()],
+  server: {
+    port: 5173, // local dev port
+  },
+  build: {
+    outDir: 'dist',
+  },
+  base: '/',
 
-})
+
+});
